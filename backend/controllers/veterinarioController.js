bb02ed9f85b2ff94a341531b1c57665d20b7f2c1
Luis Mariscal <luisferdev16@gmail.com>
2024-02-03 15:01:47 -0600

    starting project

---
+++ backend/controllers/veterinarioController.js
@@ -0,0 +1,13 @@
+
+const registrar = (req, res) => {
+    res.json({ msg: "Registrando usuario..."})
+};
+
+const perfil = (req, res) => {
+    res.json({ msg: "Mostrando perfil"})
+}
+
+export {
+    registrar,
+    perfil
+}
---
+++ backend/models/Veterinario.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const veterinarioSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required : true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    telefono: {
+        type: String,
+        default: null,
+        trim: true
+    },
+    web: {
+        type: String,
+        default: null,
+    },
+    token: {
+        type: Boolean,
+        default: null
+    }
+});
+
+const Veterinario = mongoose.model("Veterinario", veterinarioSchema);
+export default Veterinario;

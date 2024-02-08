d3af8c44c435943665bdd5c6e5d4b4aa5bb60ecb
Luis Mariscal <luisferdev16@gmail.com>
2024-02-07 23:30:23 -0600

    maquetando la autenticacion de usuarios y completando su routing

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -3,9 +3,9 @@ const Login = () => {
   return (
     <>
         <div>
-          <h1 className="text-indigo-600 font-bold text-7xl">Inicia Sesión y Administra tus <span className="text-black">Pacientes</span></h1>
+          <h1 className="text-indigo-600 font-bold text-7xl">Inicia Sesión y Administra tus{""}<span className="text-black"> Pacientes</span></h1>
         </div>
-        <div>
+        <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
           <form action="">
             <div className="my-5">
               <label
--- FrontEnd/src/pages/OlvidePassword.jsx
+++ FrontEnd/src/pages/OlvidePassword.jsx
@@ -1,8 +1,43 @@
-
+import { Link } from "react-router-dom"
 const OlvidePassword = () => {
   return (
     <>
-        <h1>Desde olvidé password</h1>
+        <div>
+          <h1 className="text-indigo-600 font-bold text-7xl">Recupera tu {""}<span className="text-black">Contraseña</span></h1>
+        </div>
+
+        <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+          <form action="">
+            <div className="my-5">
+              <label
+                className="uppercase text-gray-600 block text-xl font-bold"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                placeholder="Email de registro"
+                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              />
+            </div>
+            
+            <input
+              type="submit"
+              value={"Enviar instrucciones"}
+              className="bg-indigo-600 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+            />
+          </form>
+
+          <nav className="mt-10 lg:flex lg:justify-between">
+            <Link
+            className="block text-center my-5 text-gray-500"
+            to="/">¿Ya tienes una cuenta? Inicia Sesión</Link>
+            <Link
+            className="block text-center my-5 text-gray-500"
+            to="/registrar">¿Aún no tienes una cuenta? Regístrate</Link>
+          </nav>
+
+        </div>
     </>
   )
 }

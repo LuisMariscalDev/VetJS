120bc07b39cfa5253c06da97cfbe9c4a02595092
Luis Mariscal <luisferdev16@gmail.com>
2024-02-16 16:06:33 -0600

    comprobando el token y accediendo al form para cambiar pass

--- FrontEnd/src/pages/NuevoPassword.jsx
+++ FrontEnd/src/pages/NuevoPassword.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
-
+import { useState, useEffect } from "react";
+import {useParams} from "react-router-dom";
+import Alerta from '../components/Alerta.jsx'
+import clienteAxios from "../config/axios.jsx";
 const NuevoPassword = () => {
+
+  const [password, setPassword] = useState('');
+  const [alerta, setAlerta] = useState({});
+  const [tokenValido, setTokenValido] = useState(false);
+
+  const params = useParams();
+  const {token} = params;
+
+  useEffect(() => {
+    const comprobarToken = async () => {
+      try {
+        await clienteAxios(`/veterinarios/olvide-password/${token}`);
+        setAlerta({
+          msg: 'Coloca tu nueva contraseña',
+        })
+        setTokenValido(true);
+      } catch (error) {
+        setAlerta({
+          msg: 'Hubo un error con el enlace',
+          error: true
+        })
+      }
+    }
+    comprobarToken()
+  }, []);
+
+  const {msg} = alerta;
+
   return (
-    <h1>Desde nuevo Password</h1>
+    <>
+      <div>
+        <h1 className="text-indigo-600 font-bold text-7xl">Reestablece tu contraseña y Administra {""}<span className="text-black">tus Pacientes</span></h1>
+      </div>
+
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+
+        {msg &&  <Alerta
+          alerta={alerta}
+        />}
+
+        {tokenValido && (
+          <form>
+            <div className="my-5">
+                <label
+                  className="uppercase text-gray-600 block text-xl font-bold"
+                >
+                  Nueva Contraseña
+                </label>
+                <input
+                  type="password"
+                  placeholder="Nueva contraseña de acceso"
+                  className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                  value={password}
+                  onChange={ e => setPassword(e.target.value)}
+                />
+            </div>  
+            <input
+              type="submit"
+              value={"Guardar contraseña"}
+              className="bg-indigo-600 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+            />
+          </form> 
+        )}
+
+      </div>
+    </>
   )
 }
 

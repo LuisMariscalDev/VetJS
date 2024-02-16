2026f1c97db173736456bb389595a35104b45647
Luis Mariscal <luisferdev16@gmail.com>
2024-02-16 17:57:33 -0600

    iniciando con el context provider

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -5,11 +5,14 @@ import Registrar from './pages/Registrar';
 import OlvidePassword from './pages/OlvidePassword';
 import ConfirmarCuenta from './pages/ConfirmarCuenta';
 import NuevoPassword from './pages/NuevoPassword';
+
+import { AuthProvider } from './context/AuthProvider';
 function App() {
 
   return (
       <BrowserRouter>
-          <Routes>
+        <AuthProvider>
+         <Routes>
             <Route path='/' element={<AuthLayout/>}>
               <Route index element={<Login/>}/>
               <Route path='registrar' element={<Registrar/>}/>
@@ -18,6 +21,7 @@ function App() {
               <Route path='confirmar/:id' element={<ConfirmarCuenta/>}/>
             </Route>
           </Routes>
+        </AuthProvider>
       </BrowserRouter>
   )
 }

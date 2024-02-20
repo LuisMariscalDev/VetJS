355fe0bbf98ee51ac6b7446bf9c85b4b51820ab2
Luis Mariscal <luisferdev16@gmail.com>
2024-02-19 18:07:03 -0600

    iniciando con las rutas protegidas

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AuthLayout from './layout/AuthLayout';
+import RutaProtegida from './layout/RutaProtegida';
 import Login from './pages/Login';
 import Registrar from './pages/Registrar';
 import OlvidePassword from './pages/OlvidePassword';
@@ -7,6 +8,7 @@ import ConfirmarCuenta from './pages/ConfirmarCuenta';
 import NuevoPassword from './pages/NuevoPassword';
 
 import { AuthProvider } from './context/AuthProvider';
+import AdministrarPacientes from './pages/AdministrarPacientes';
 function App() {
 
   return (
@@ -20,6 +22,12 @@ function App() {
               <Route path='olvide-password/:token' element={<NuevoPassword/>}/>
               <Route path='confirmar/:id' element={<ConfirmarCuenta/>}/>
             </Route>
+
+            <Route path="/admin" element={<RutaProtegida/>}>
+              <Route index element={<AdministrarPacientes/>}/>
+
+            </Route>
+
           </Routes>
         </AuthProvider>
       </BrowserRouter>
---
+++ FrontEnd/src/pages/AdministrarPacientes.jsx
@@ -0,0 +1,7 @@
+const AdministrarPacientes = () => {
+  return (
+    <h1>Administrando pacientes</h1>
+  )
+}
+
+export default AdministrarPacientes
